57bb36a4e858402f1c1cad3ab090bc6da25ab1f5
Mohamed Asik <mail2asik@gmail.com>
2026-07-19 13:38:25 +0000

    Main Application Shell, acts as core structural framework component where Vue Router will inject components matching our address bar route.

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <!-- Router View renders the active page component based on URL -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+git 
